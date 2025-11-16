35c85ff3a09d0c53ec696814521ae555ca04fba9
Sasha Bemotoff <sashabemotoff@Sashas-MBP.fritz.box>
2025-11-16 19:49:10 +0100

    TEST: Replace commas with exclamation marks

--- frontend/app/components/homepage/hero-section/index.jsx
+++ frontend/app/components/homepage/hero-section/index.jsx
@@ -49,9 +49,9 @@ const HeroSection = ({ data }) => {
       <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
         <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
           <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
-            {t('hero.hello')}<br />
+            {t('hero.hello')}!<br />
             {t('hero.thisIs')} <span className=" text-pink-500">{name}</span>
-            {` ${t('hero.imA')} `}
+            {`! ${t('hero.imA')} `}
             <span className=" text-[#16f2b3]">{designation}</span>.
           </h1>
 
